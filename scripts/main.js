91b50cadbe39ad68f19f20b54662e255b094cb6b
DphnZwp <zwuupdaphne@gmail.com>
2022-02-18 16:25:32 +0100

    Keydown
    Keydown werkend gemaakt

--- scripts/main.js
+++ scripts/main.js
@@ -1,21 +1,22 @@
-const buttons = document.querySelectorAll("a")
+const buttons = document.querySelectorAll('a')
 
 buttons.forEach(button => {
 
 	// Single click event listener
 	button.addEventListener('click', function() {
-		button.classList.add("purple")
+		button.classList.add('purple')
 	})
 
 	// Double click event listener
 	button.addEventListener('dblclick', function() { 
-		button.classList.add("cyan")
+		button.classList.add('cyan')
 	})
 
 	// Keydown event listener
-	button.addEventListener('keydown', function() { 
-		if (event.key === 'd') {
-			button.classList.add = "remove-all";
+	window.addEventListener('keydown', function(event) { 
+		if (event.key === 's') {
+			button.classList.remove('purple')
+			button.classList.remove('cyan')
 		}
 	})
 })
